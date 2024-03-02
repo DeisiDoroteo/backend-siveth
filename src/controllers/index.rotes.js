042c450f57513506eca6401050d69c00f6f21538
DeisiDoroteo <siveth.uthh03@gmail.com>
2024-03-02 13:54:34 -0600

    ggg

--- src/controllers/index.rotes.js
+++ src/controllers/index.rotes.js
@@ -2,11 +2,11 @@ import { pool } from "../db.js";
 
 export const index = (req, res) => res.json({ message: "welcome to my api" });
 export const ping = async (req, res) => {
- try {
- const [result] = await pool.query('SELECT "pong" as result');
- res.json(result[0]);
- } catch (error) {
- console.error("Error while pinging the database:", error);
- res.status(500).json({ message: "Internal Server Error" });
- }
+  try {
+    const [result] = await pool.query('SELECT "pong" as result');
+    res.json(result[0]);
+  } catch (error) {
+    console.error("Error while pinging the database:", error);
+    res.status(500).json({ message: "Internal Server Error", error: error.message });
+  }
 };
