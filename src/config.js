8ec22b2bf542054aeb02fac91c69e59c56f97ef6
DeisiDoroteo <siveth.uthh03@gmail.com>
2024-03-02 18:01:26 -0600

    prueba

--- src/config.js
+++ src/config.js
@@ -4,14 +4,14 @@ config();
 export const PORT = process.env.PORT || 3001;
 export const DB_HOST = process.env.DB_HOST || '127.0.0.1:3306';
 export const DB_USER = process.env.DB_USER || 'u233332198_BD_Siveth';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'iveth28032003*';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'Siveth28032003*';
 export const DB_DATABASE = process.env.DB_DATABASE || 'u233332198_BD_Siveth';
 export const DB_PORT = process.env.DB_PORT || 3306;
 
 
 // export const PORT = process.env.PORT || 3000;
-// export const DB_HOST = process.env.DB_HOST || 'uk-fast-web1216.main-hosting.eu';
-// export const DB_USER = process.env.DB_USER || 'u233332198_BD_Siveth';
-// export const DB_PASSWORD = process.env.DB_PASSWORD || 'Siveth28032003*';
-// export const DB_DATABASE = process.env.DB_DATABASE || 'u233332198_BD_Siveth';
+// export const DB_HOST = process.env.DB_HOST || 'localhost';
+// export const DB_USER = process.env.DB_USER || 'root';
+// export const DB_PASSWORD = process.env.DB_PASSWORD || '';
+// export const DB_DATABASE = process.env.DB_DATABASE || 'siveth';
 // export const DB_PORT = process.env.DB_PORT || 3306;
