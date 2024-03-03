96314a3ab9c5c4bfebf0296d552d3b4b7e9d16b1
DeisiDoroteo <siveth.uthh03@gmail.com>
2024-03-02 19:18:12 -0600

    db config

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import {
  DB_DATABASE,
  DB_HOST,
  DB_PASSWORD,
- DB_PORT,
+//  DB_PORT,
  DB_USER,
 } from "./config.js";
 
@@ -11,6 +11,6 @@ export const pool = createPool({
  host: DB_HOST,
  user: DB_USER,
  password: DB_PASSWORD,
- port: DB_PORT,
+//  port: DB_PORT,
  database: DB_DATABASE,
 });
