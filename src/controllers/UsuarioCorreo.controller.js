abed4b34613c6599a4fa8c2098ef66f511f21a8e
DeisiDoroteo <siveth.uthh03@gmail.com>
2024-03-03 00:06:15 -0600

    usuario

--- src/controllers/UsuarioCorreo.controller.js
+++ src/controllers/UsuarioCorreo.controller.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 import { pool } from "../db.js";
-
+import bcrypt from 'bcrypt';
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -21,11 +21,11 @@ export const AsignarCodigo = async (req, res) => {
     if (selectResults.length > 0) {
       const updateQuery = 'UPDATE codepass SET codigo = ? WHERE fk_usuario = ?';
       await pool.query(updateQuery, [randomCode, correo]);
-      res.json({ status: 'success', message: 'Código aleatorio actualizado con éxito', code: randomCode });
+      res.json({ status: 'éxito', message: 'Código aleatorio actualizado con éxito', code: randomCode });
     } else {
       const insertQuery = 'INSERT INTO codepass (fk_usuario, codigo) VALUES (?, ?)';
       await pool.query(insertQuery, [correo, randomCode]);
-      res.json({ status: 'success', message: 'Código aleatorio asignado con éxito', code: randomCode });
+      res.json({ status: 'éxito', message: 'Código aleatorio asignado con éxito', code: randomCode });
     }
   } catch (error) {
     console.error('Error al asignar código aleatorio:', error);
@@ -33,20 +33,16 @@ export const AsignarCodigo = async (req, res) => {
   }
 };
 
-
-
 export const enviarCorreoRecuperacion = async (req, res) => {
   try {
     const { recipient_email } = req.body;
 
-    // Consultar el código de recuperación en la base de datos
     const query = 'SELECT codigo FROM codepass WHERE fk_usuario = ?';
     const [results] = await pool.query(query, [recipient_email]);
 
     if (results.length > 0) {
       const OTP = results[0].codigo;
 
-      // Configurar el correo electrónico
       const mailOptions = {
         from: 'siveth.uthh03@gmail.com', // Cambiar con tu dirección de correo electrónico de Gmail
         to: recipient_email,
@@ -54,7 +50,6 @@ export const enviarCorreoRecuperacion = async (req, res) => {
         text: `Hola, acabas de recibir tu código de restablecimiento de contraseña. Por favor, ten cuidado y no lo compartas con nadie. Tu código es: ${OTP}`
       };
 
-      // Enviar el correo electrónico usando el transporte SMTP de Gmail
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
           console.error('Error al enviar el correo electrónico de recuperación:', error);
@@ -65,7 +60,6 @@ export const enviarCorreoRecuperacion = async (req, res) => {
         }
       });
     } else {
-      // El usuario no tiene un código de recuperación asociado en la base de datos
       return res.status(404).json({ message: 'No se encontró un código de recuperación asociado a este usuario.' });
     }
   } catch (error) {
@@ -78,15 +72,12 @@ export const verificarCodigoRecuperacion = async (req, res) => {
   try {
     const { correo, codigo } = req.body;
 
-    // Consultar el código de recuperación en la base de datos
     const query = 'SELECT * FROM codepass WHERE fk_usuario = ? AND codigo = ?';
     const [results] = await pool.query(query, [correo, codigo]);
 
     if (results.length > 0) {
-      // Si se encuentra un código coincidente, redirigir al usuario a otra pantalla
       return res.json({ message: 'Código de recuperación válido. Redirigiendo...' });
     } else {
-      // Si no se encuentra un código coincidente, devolver un mensaje de error
       return res.status(404).json({ message: 'El código de recuperación no es válido.' });
     }
   } catch (error) {
@@ -99,27 +90,15 @@ export const cambiarContrasenia = async (req, res) => {
   try {
     const { correo, contraseniaNueva } = req.body;
 
-    // Generar hash de la contraseña nueva
-    bcrypt.hash(contraseniaNueva, 10, async (err, hashedPassword) => {
-      if (err) {
-        console.error("Error al generar hash de la contraseña nueva:", err);
-        return res.status(500).send("Error al actualizar la contraseña");
-      }
+    const hashedPassword = await bcrypt.hash(contraseniaNueva, 10);
 
-      try {
-        // Actualizar la contraseña en la base de datos con el hash generado
-        const sql = "UPDATE usuarios SET Password = ? WHERE Correo = ?";
-        await pool.query(sql, [hashedPassword, correo]);
+    const sql = "UPDATE usuarios SET Password = ? WHERE Correo = ?";
+    await pool.query(sql, [hashedPassword, correo]);
 
-        console.log("Contraseña actualizada correctamente");
-        return res.status(200).send("Contraseña actualizada correctamente");
-      } catch (error) {
-        console.error("Error al actualizar la contraseña:", error);
-        return res.status(500).send("Error al actualizar la contraseña");
-      }
-    });
+    console.log("Contraseña actualizada correctamente");
+    return res.status(200).send("Contraseña actualizada correctamente");
   } catch (error) {
-    console.error("Error:", error);
-    return res.status(500).send("Something went wrong");
+    console.error("Error al actualizar la contraseña:", error);
+    return res.status(500).send("Error al actualizar la contraseña");
   }
 };
