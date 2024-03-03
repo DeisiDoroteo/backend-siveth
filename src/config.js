39461122daf4e90106afd25be98839fa9218f60f
DeisiDoroteo <siveth.uthh03@gmail.com>
2024-03-02 19:00:05 -0600

    config siveth

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3001;
-export const DB_HOST = process.env.DB_HOST || 'viajeespeciales.com';
+export const DB_HOST = process.env.DB_HOST || 'localhost';
 // export const DB_HOST = process.env.DB_HOST || 'uk-fast-web1216.main-hosting.eu';
 export const DB_USER = process.env.DB_USER || 'u233332198_BD_Siveth';
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'Siveth28032003*';
