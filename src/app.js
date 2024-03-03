b2bfa2a438eaf32d9ef30a0071176d840feaec63
DeisiDoroteo <siveth.uthh03@gmail.com>
2024-03-03 01:41:13 -0600

    app config

--- src/app.js
+++ src/app.js
@@ -17,7 +17,9 @@ app.use(express.json());
 app.use("/", indexRoutes);
 app.use("/ping", indexRoutes);
 app.use("/api", usuariosRoutes);
-app.use("/api", CorreoRoutes);
+app.use("/api", usuariosRoutes);
+app.use("/api", usuariosRoutes);
+app.use("/api", usuariosRoutes);
 
 app.use((req, res, next) => {
 res.status(404).json({ message: "Not found" });
